d9d3a64b6c5f0e471c4cdfa674de209fadf607c5
Merielle Impreso <merielle@aeustech.com>
2018-01-22 22:45:07 +0800

    Trying inital client with react

--- public/main.js
+++ public/main.js
@@ -1,25 +1,11 @@
 function Application() {
   return <div>
-    <h1>React does not have to be complicated.</h1>
-    <p>
-      In fact, the React app that you are looking at only
-      has three files:
-    </p>
-    <ul>
-      <li><code>index.html</code> loads the necessary scripts.</li>
-      <li><code>main.js</code> renders this message.</li>
-      <li><code>main.css</code> just sets the font.</li>
-    </ul>
-    <p>
-      Of course, JSX needs to be compiled. But React Armorys editor
-      does that for you - you can see the result by clicking "Compiled"
-      at the bottom of the editor.
-    </p>
-    <p>
-      <strong>It is never been easier to try React!</strong> All you need
-      to do is change a few words in <code>main.js</code>, and your changes will
-      be displayed instantly!
-    </p>
+    <h1>Products Grid</h1>
+    <p>Here you are sure to find a bargain on some of the finest ascii available to purchase. Be sure to peruse our selection of ascii faces in an exciting range of sizes and prices.</p>
+
+    <section class="products">
+        ... products go here ...
+    </section>
   </div>
 }
 
