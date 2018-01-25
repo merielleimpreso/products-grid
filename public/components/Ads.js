c389319e282d3ee53b8d1fb28edf0ad478195d10
Merielle Impreso <merielle@aeustech.com>
2018-01-25 12:14:02 +0800

    Fixed Ads script

--- public/components/Ads.js
+++ public/components/Ads.js
@@ -1,15 +1,17 @@
 import React from 'react';
 
-const Ads = ({source}) => {
-  let randomNumber = Math.floor(Math.random()*1000);
-  let sourceUrl = source || `http://localhost:3000/ads/?r=${randomNumber}`;
-  return (
-    <img
-      alt={randomNumber}
-      style={{alignSelf: 'center', justifyContent: 'space-around', alignItems:'center', height:200}}
-      src={sourceUrl}
-    />
-  );
+class Ads extends React.Component {
+  render() {
+    let randomNumber = Math.floor(Math.random()*1000);
+    let sourceUrl = (this.props.source) ? this.props.source : `http://localhost:3000/ads/?r=${randomNumber}`;
+    return (
+      <img
+        alt={randomNumber}
+        style={{alignSelf: 'center', justifyContent: 'space-around', alignItems:'center', height:200}}
+        src={sourceUrl}
+      />
+    );
+  }
 }
 
 export default Ads;
