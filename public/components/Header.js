5c7822be79b38796f93a2ece34e1b0f6d386dd0a
Merielle Impreso <merielle@aeustech.com>
2018-01-25 14:27:46 +0800

    Added header text

--- public/components/Header.js
+++ public/components/Header.js
@@ -9,16 +9,22 @@ import Typography from 'material-ui/Typography';
 import { withStyles } from 'material-ui/styles';
 
 const styles = theme => ({
-  flex: {
-    flex: 1,
+  title: {
+    flex:1,
+    marginRight: 100,
   },
   formControl: {
     margin: theme.spacing.unit,
     minWidth: 120,
   },
-  selectEmpty: {
-    marginTop: theme.spacing.unit * 2,
+  text: {
+    flex:1,
+    textAlign: 'left',
+    color: '#1A237E'
   },
+  subtitle: {
+    marginRight: 30
+  }
 });
 
 class Header extends React.Component {
@@ -41,12 +47,19 @@ class Header extends React.Component {
     return (
       <AppBar>
         <Toolbar>
-          <Typography type="title" color="inherit" className={classes.flex}>Products Grid</Typography>
+          <Typography type="headline" color="inherit" className={classes.title}>Products Grid</Typography>
+          <Typography type="caption" color="inherit" className={classes.subtitle}>
+            <em>
+              Here you are sure to find a bargain on some of the finest ascii available to purchase.<br />
+              Be sure to peruse our selection of ascii faces in an exciting range of sizes and prices.
+            </em>
+          </Typography>
           <div>
             <FormControl className={classes.formControl}>
               <InputLabel htmlFor="age-simple">Sort by</InputLabel>
               <Select value={this.state.sortBy}
                 onChange={this.handleChange}
+                style={{textAlign:'center'}}
                 inputProps={{
                   name: 'sortBy',
                   id: 'sortBy-simple',
